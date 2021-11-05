b0913ab021805586742483b4e42fbf02343f2635
Ibrohim Rasulov <ibrohim05062000@gmail.com>
2021-11-05 18:52:33 +0500

    Expand function and add test for that

--- stringLength.js
+++ stringLength.js
@@ -1,5 +1,9 @@
 function stringLength(str) {
-  return str.length;
+  if (str.length > 0 && str.length < 10) {
+    return str.length;
+  } else {
+    throw new Error('Check fail: str length < 1 or > 10')
+  }
 }
 
 module.exports = stringLength;
