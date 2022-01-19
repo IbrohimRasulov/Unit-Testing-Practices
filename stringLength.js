06b18123af939780be2fc9a44c3f470876e4a017
Ibrohim Rasulov <ibrohim05062000@gmail.com>
2022-01-19 23:03:56 +0500

    Resolve task 1

--- stringLength.js
+++ stringLength.js
@@ -1,9 +1,5 @@
 function stringLength(str) {
-  if (str.length > 0 && str.length < 10) {
-    return str.length;
-  } else {
-    throw new Error('Check fail: str length < 1 or > 10')
-  }
+  return str.length;
 }
 
 module.exports = stringLength;
